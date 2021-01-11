4914dd7cc86016d73a1644951402212106ec5cc3
YIHAO DING <37336043+Dean930610@users.noreply.github.com>
2021-01-11 20:28:33 +1100

    Update io.py

--- detectron/utils/io.py
+++ detectron/utils/io.py
@@ -33,7 +33,7 @@ from uuid import uuid4
 
 logger = logging.getLogger(__name__)
 
-_DETECTRON_S3_BASE_URL = 'https://drive.google.com/file'
+_DETECTRON_S3_BASE_URL = 'https://drive.google.com/file/d/1uIdxMuoYbJ0HLoQIo3c36EyPme3cdhXs/'
 
 
 def save_object(obj, file_name, pickle_format=2):
